f95ff88fb5303bf405b142d4a29507fd3b3922ff
Erik Somnäs <erik.somnas@gmail.com>
2022-10-18 08:34:49 +0200

    fetch external data through graphql

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,26 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
+//import { getSortedPostsData } from '../lib/posts';
+import { getPosts } from '../services';
 
-export async function getStaticProps() {
+/* export async function getStaticProps() {
 	const allPostsData = getSortedPostsData();
 	return {
 		props: {
 			allPostsData,
 		},
 	};
+} */
+
+export async function getStaticProps() {
+    const posts = (await getPosts()) || [];
+    return {
+        props: { posts },
+    };
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({ allPostsData, posts }) {
 	return (
 		<Layout home>
 			<Head>
@@ -28,13 +36,22 @@ export default function Home({ allPostsData }) {
 			<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
 				<h2 className={utilStyles.headingLg}>Blog</h2>
 				<ul className={utilStyles.list}>
-					{allPostsData.map(({ id, date, title }) => (
+					{/* {allPostsData.map(({ id, date, title }) => (
 						<li className={utilStyles.listItem} key={id}>
 							{title}
 							<br />
 							{id}
 							<br />
 							{date}
+						</li>
+					))} */}
+					{posts.map((post, index) => (
+						<li className={utilStyles.listItem} key={index}>
+							{post.node.title}
+							<br />
+							{post.node.excerpt}
+							<br />
+							{post.node.createdAt}
 						</li>
 					))}
 				</ul>
